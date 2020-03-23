a2843fad52503d01b8b36d00d518ef449b8ba0ee
HuyTV <tranvanhuy0202@gmail.com>
2020-03-24 01:16:57 +0700

    laravel admin

---
+++ app/Admin/Controllers/ProductController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Category;
+use App\Product;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ProductController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Product';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Product());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'));
+        $grid->column('price', __('Price'));
+        $grid->column('image', __('Image'))->image();
+//        $grid->column('category_id', __('Category id'));
+        $grid->category()->name();
+        $grid->column('created_at', __('Created at'))->sortable();
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Product::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('price', __('Price'));
+        $show->field('image', __('Image'));
+        $show->field('category_id', __('Category id'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Product());
+
+        $form->text('name', __('Name'));
+        $form->number('price', __('Price'));
+        $form->image('image', __('Image'));
+//        $form->number('category_id', __('Category id'));
+        $form->select('category_id', __('Category name'))->options(Category::all()->pluck('name', 'id'));
+
+        return $form;
+    }
+}
